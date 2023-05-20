605fccd8a4333434c90fedaffbc7127ddf6e57ee
GANDUWARD <111099929+GANDUWARD@users.noreply.github.com>
2023-05-20 22:08:52 +0800

    first upload for saving

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,14 +1,19 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+)
 
+//import "os"
 
 type Coordinator struct {
 	// Your definitions here.
+	Allfiles      []string // 所有文件名
+	NReduce       int      // 用于安排Reduce任务的个数
+	Havetaskindex int      //进行安排的任务个数
 
 }
 
@@ -19,11 +24,19 @@ type Coordinator struct {
 //
 // the RPC argument and reply types are defined in rpc.go.
 //
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+//func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
+//	reply.Y = args.X + 1
+//	return nil
+//}
+func (c *Coordinator) SendFile(args *MyArgs, reply *MyReply) error { //参数：命令类型 回复：尚未执行map操作的文件名
+	if args == "map" {
+		if c.Havetaskindex < len(c.Allfiles) {
+			reply = c.Allfiles[c.Havetaskindex]
+			c.Havetaskindex++
+		}
+	}
 	return nil
 }
-
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -31,10 +44,10 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
-	//l, e := net.Listen("tcp", ":1234")
-	sockname := coordinatorSock()
-	os.Remove(sockname)
-	l, e := net.Listen("unix", sockname)
+	l, e := net.Listen("tcp", ":1234")
+	//sockname := coordinatorSock()
+	//os.Remove(sockname)
+	//l, e := net.Listen("unix", sockname)
 	if e != nil {
 		log.Fatal("listen error:", e)
 	}
@@ -49,8 +62,10 @@ func (c *Coordinator) Done() bool {
 	ret := false
 
 	// Your code here.
-
-
+	//如果任务全部完成,就返回真
+	if c.Havetaskindex == len(c.Allfiles) {
+		ret = true
+	}
 	return ret
 }
 
@@ -61,10 +76,10 @@ func (c *Coordinator) Done() bool {
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
-
 	// Your code here.
-
-
+	c.Havetaskindex = 0
+	c.Allfiles = files
+	c.NReduce = nReduce
 	c.server()
 	return &c
 }
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,11 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
-
+import (
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+)
 
 //
 // Map functions return a slice of KeyValue.
@@ -24,7 +25,6 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 //
 // main/mrworker.go calls this function.
 //
@@ -32,7 +32,8 @@ func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
-
+	MapCall(mapf)
+	ReduceCall(reducef)
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
 
@@ -43,29 +44,36 @@ func Worker(mapf func(string, string) []KeyValue,
 //
 // the RPC argument and reply types are defined in rpc.go.
 //
-func CallExample() {
+func MapCall(mapf func(string, string) []KeyValue) {
 
-	// declare an argument structure.
-	args := ExampleArgs{}
-
-	// fill in the argument(s).
-	args.X = 99
-
-	// declare a reply structure.
-	reply := ExampleReply{}
-
-	// send the RPC request, wait for the reply.
-	// the "Coordinator.Example" tells the
-	// receiving server that we'd like to call
-	// the Example() method of struct Coordinator.
-	ok := call("Coordinator.Example", &args, &reply)
-	if ok {
-		// reply.Y should be 100.
-		fmt.Printf("reply.Y %v\n", reply.Y)
-	} else {
-		fmt.Printf("call failed!\n")
-	}
 }
+func ReduceCall(reducef func(string, []string) string) {
+
+}
+
+//func CallExample() {
+//
+//	// declare an argument structure.
+//	args := ExampleArgs{}
+//
+//	// fill in the argument(s).
+//	args.X = 99
+//
+//	// declare a reply structure.
+//	reply := ExampleReply{}
+//
+//	// send the RPC request, wait for the reply.
+//	// the "Coordinator.Example" tells the
+//	// receiving server that we'd like to call
+//	// the Example() method of struct Coordinator.
+//	ok := call("Coordinator.Example", &args, &reply)
+//	if ok {
+//		// reply.Y should be 100.
+//		fmt.Printf("reply.Y %v\n", reply.Y)
+//	} else {
+//		fmt.Printf("call failed!\n")
+//	}
+//}
 
 //
 // send an RPC request to the coordinator, wait for the response.
@@ -73,9 +81,9 @@ func CallExample() {
 // returns false if something goes wrong.
 //
 func call(rpcname string, args interface{}, reply interface{}) bool {
-	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
-	sockname := coordinatorSock()
-	c, err := rpc.DialHTTP("unix", sockname)
+	c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
+	//sockname := coordinatorSock()
+	//c, err := rpc.DialHTTP("unix", sockname)
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
@@ -85,7 +93,6 @@ func call(rpcname string, args interface{}, reply interface{}) bool {
 	if err == nil {
 		return true
 	}
-
 	fmt.Println(err)
 	return false
 }
